b2c82e216b2d3ccc40b5b3b72b8342d928f6423d
Chris Agold <christophersagold@gmail.com>
2021-03-01 15:54:32 -0600

    completed join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,48 +1,37 @@
-DROP DATABASE IF EXISTS join_test_db;
-CREATE DATABASE IF NOT EXISTS join_test_db;
-USE join_test_db;
+USE employees;
+# write a query that shows each department along with the name of the current manager for that department.
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments as d
+JOIN dept_manager dm on d.dept_no = dm.dept_no
+JOIN employees e on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
 
-CREATE TABLE IF NOT EXISTS roles
-(
-    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);
+# Find the name of all departments currently managed by women
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments as d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+ORDER BY d.dept_name;
 
-CREATE TABLE IF NOT EXISTS users
-(
-    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(100) NOT NULL,
-    email   VARCHAR(100) NOT NULL,
-    role_id INT UNSIGNED DEFAULT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id)
-);
+# Find the current titles of employees currently working in the Customer Service department.
+SELECT t.title AS 'Title', COUNT(*) AS 'Head Count'
+FROM employees e
+JOIN dept_emp de on e.emp_no = de.emp_no
+JOIN departments d on de.dept_no = d.dept_no
+JOIN titles t on e.emp_no = t.emp_no
+WHERE de.to_date > NOW() AND t.to_date > NOW() AND d.dept_name = 'Customer Service'
+GROUP BY title;
 
-INSERT INTO roles (name) VALUES ('admin');
-INSERT INTO roles (name) VALUES ('author');
-INSERT INTO roles (name) VALUES ('reviewer');
-INSERT INTO roles (name) VALUES ('commenter');
-
-INSERT INTO users (name, email, role_id) VALUES
-('bob', 'bob@example.com', 1),
-('adam', 'adam@example.com', 2),
-('vitold', 'vitold@example.com', 3),
-('jonas', 'jonas@example.com', 3),
-('sam', 'sam@example.com', null),
-('jim', 'jim@example.com', null),
-('buford', 'buford@example.com', null),
-('den', 'den@example.com', 2),
-('long', 'long@example.com', 2),
-('tim', 'tim@example.com', 2);
-
-DESCRIBE users;
-DESCRIBE roles;
-
-SELECT * FROM users;
-SELECT * FROM roles;
-
-
+# Find the current salary of all current managers.
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees e
+         JOIN dept_emp dm on e.emp_no = dm.emp_no
+         JOIN departments d on dm.dept_no = d.dept_no
+         JOIN titles s on e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY d.dept_name;
 
 
 
---
+++ join_test.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS join_test_db;
+CREATE DATABASE IF NOT EXISTS join_test_db;
+USE join_test_db;
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(100) NOT NULL,
+    email   VARCHAR(100) NOT NULL,
+    role_id INT UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+('bob', 'bob@example.com', 1),
+('adam', 'adam@example.com', 2),
+('vitold', 'vitold@example.com', 3),
+('jonas', 'jonas@example.com', 3),
+('sam', 'sam@example.com', null),
+('jim', 'jim@example.com', null),
+('buford', 'buford@example.com', null),
+('den', 'den@example.com', 2),
+('long', 'long@example.com', 2),
+('tim', 'tim@example.com', 2);
+
+# DESCRIBE users;
+# DESCRIBE roles;
+# SELECT * FROM users;
+# SELECT * FROM roles;
+
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+JOIN roles ON users.role_id = role_id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
+
+
+
