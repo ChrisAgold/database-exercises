0dbfa0a2285bc26640aac1946efe89e8bd01939f
Chris Agold <christophersagold@gmail.com>
2021-03-02 12:01:30 -0600

    added subquery exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,6 +7,28 @@ WHERE hire_date IN (
     WHERE emp_no = 101010
 );
 
+-- # SELECT t.title AS 'Titles'
+-- # FROM titles t
+-- # WHERE t.emp_no IN (
+-- #     SELECT e.emp_no
+-- #     FROM employees e
+-- #     WHERE e.first_name = 'Aamod'
+-- # );
 
+SELECT title, COUNT(title)
+FROM titles
+WHERE emp_no IN (
+SELECT emp_no
+FROM employees
+WHERE first_name = 'Amond'
+)
+GROUP BY title;
 
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'F' AND emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+);
 
